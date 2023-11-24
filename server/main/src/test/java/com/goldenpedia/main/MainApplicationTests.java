fb15ab79007ddab13a343aa73e1fe1d7b815a1dc
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2023-11-24 16:33:55 -0300

    feat: testa getWordsById do WordsController Co-authored-by: Lucca <Lucca08@users.noreply.github.com> Co-authored-by: Amós Lima <social.amos@gmail.com>

--- server/main/src/test/java/com/goldenpedia/main/MainApplicationTests.java
+++ server/main/src/test/java/com/goldenpedia/main/MainApplicationTests.java
@@ -1,14 +1,5 @@
 package com.goldenpedia.main;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
-
 class MainApplicationTests {
-
-	@Test
-	public void testAdd() {
-		assertEquals(42, Integer.sum(19, 23));
-	}
 
 }
--- server/main/src/test/java/com/goldenpedia/main/controllers/WordControllerTest.java
+++ server/main/src/test/java/com/goldenpedia/main/controllers/WordControllerTest.java
@@ -1,6 +1,58 @@
 package com.goldenpedia.main.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.goldenpedia.main.repository.WordRepository;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@WebMvcTest(controllers = WordController.class)
+@ExtendWith(MockitoExtension.class)
 public class WordControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @MockBean
+    private WordRepository wordRepository;
+
+    @Test
+    @DisplayName("Should return status 200 (Ok) when a valid status is passed")
+    public void WordController_GetWordsByStatus_ReturnOk() throws Exception{
+        MockHttpServletResponse response = this.mockMvc
+        .perform(MockMvcRequestBuilders.get("/words/{status}", "Revisar")
+        .contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("Should return status 404 (Not Found) when invalid word id is passed")
+    public void WordController_GetWordsByStatus_ReturnNotFound() throws Exception{
+        MockHttpServletResponse response = this.mockMvc
+        .perform(MockMvcRequestBuilders.get("/words")
+        .param("wordId", "1")
+        .contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    @DisplayName("Should return status 400 (Bad Request) when no status or word id is passed")
+    public void WordController_GetWordsByStatus_ReturnBadRequest() throws Exception{
+        MockHttpServletResponse response = this.mockMvc
+        .perform(MockMvcRequestBuilders.get("/words")
+        .contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 }
